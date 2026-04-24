9eb8a13e23c475b1e4f42f2635e673f0bf456423
Sergey Ermakov <rihlerstudy@gmail.com>
2026-04-24 17:57:17 +0300

    add: DTO-output for Training model - TrainingOutput

---
+++ API-sport-training-program/Models/TrainingOutput.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_sprot_training_program.Models
+{
+    public class TrainingOutput
+    {
+        public string Id { get; set; }
+
+        public TrainingType Specializaion { get; set; }
+
+        public String Title { get; set; }
+
+        public Difficulty Level { get; set; }
+
+        public string IdCoach { get; set; }
+
+        public Decimal Price { get; set; }
+
+    }
+}
