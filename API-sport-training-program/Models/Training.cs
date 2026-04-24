76f64f5f538bb7350b12deb33f1a5d5358b432a7
Sergey Ermakov <rihlerstudy@gmail.com>
2026-04-24 16:24:49 +0300

    add: Training model

--- API-sport-training-program/Models/Training.cs
+++ API-sport-training-program/Models/Training.cs
@@ -1,6 +1,32 @@
 ﻿namespace API_sprot_training_program.Models
 {
+    public enum TrainingType
+    {
+        Cardio,
+        Power,
+        Functional,
+        Dance,
+        Yoga,
+        Pilates
+    }
+
+    public enum Difficulty
+    {
+        Low,
+        Middle,
+        High
+    }
     public class Training
     {
+        public string Id { get; set; }
+        public TrainingType Specializaion { get; set; }
+        public String Title { get; set; }
+
+        public Difficulty Level { get; set; }
+
+        public string IdCoach {  get; set; }
+
+        public Decimal Price {  get; set; }
+
     }
 }
