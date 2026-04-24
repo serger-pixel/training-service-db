a48ce7d85ab68ad30a72d86754fd380e389b68cb
Sergey Ermakov <rihlerstudy@gmail.com>
2026-04-24 17:18:56 +0300

    add: DTO-input for Training model - TrainingInput
    feat: Validation for TrainingInput
    
    refactor: Validation struct

---
+++ API-sport-training-program/Models/TrainingInput.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_sprot_training_program.Models
+{
+
+    public class TrainingInput
+    {
+        [Required]
+        [EnumDataType(typeof(TrainingType))]
+        public TrainingType Specializaion { get; set; }
+
+        [Required]
+        [MaxLength(Validation.MAX_LEN_STRING)]
+        [MinLength(Validation.MIN_LEN_STRING)]
+        public String Title { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(TrainingType))]
+        public Difficulty Level { get; set; }
+
+        [Required]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string IdCoach { get; set; }
+
+        [Required]
+        [Range(typeof(Decimal), Validation.MIN_PRICE, Validation.MAX_PRICE)]
+        public Decimal Price { get; set; }
+
+    }
+}
--- API-sport-training-program/Models/Validation.cs
+++ API-sport-training-program/Models/Validation.cs
@@ -2,13 +2,8 @@
 {
     public struct Validation
     {
-        public const int MAX_CNT_DURATION_IN_WEEK = 7;
-        public const int MIN_CNT_DURATION_IN_WEEK = 1;
-
-
-        public const int MAX_CNT_IN_WEEK = 52;
-        public const int MIN_CNT_IN_WEEK = 1;
-
+        public const string MIN_PRICE = "100.0";
+        public const string MAX_PRICE = "1000.0";
 
         public const int MAX_LEN_STRING = 255;
         public const int MIN_LEN_STRING = 5;
