3374e0836a6ba2ec30bca6ea3ca5356c6d68218c
Sergey Ermakov <rihlerstudy@gmail.com>
2026-05-10 18:52:03 +0300

    feat(Models): ProducerMessage.cs, ConsumerMessage.cs
    Adding messages for broker

---
+++ Models/ConsumerMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace  training_service_db.Models
+{
+    public class ConsumerMessage
+    {
+        public string CoachId { get; set; }
+        public string UserId { get; set;}
+        public string TimeConfirm { get; set; }
+    }
+}
---
+++ Models/ProducerMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace training_service_db.Models
+{
+    public class ProducerMessage
+    {
+        public string UserId { get; set; }
+        public string CoachId { get; set; }
+    }
+}
